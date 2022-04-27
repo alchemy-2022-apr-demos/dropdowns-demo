212866c022ec237a8d0b2146ae520f5d0d9c177f
Julie Nisbet <jmnisbet@gmail.com>
2022-04-27 14:45:12 -0700

    adding selects and displayStats function

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-// import functions and grab DOM elements
+// Get our Elements
+const selectElem = document.getElementById('animal-select');
+const zodiacSelect = document.getElementById('zodiac-select');
+console.log(zodiacSelect);
 
-// let state
+const animalDiv = document.getElementById('animal');
+const zodiacDiv = document.getElementById('zodiac');
+const countElem = document.getElementById('count');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+let animalCount = 0;
+let zodiacCount = 0;
+
+// listening for any changes to the select menu
+selectElem.addEventListener('change', () => {
+    console.log('changing select', selectElem.value);
+    animalDiv.style.backgroundImage = `url('./assets/${selectElem.value}.png')`;
+    // animalDiv.style = `background-image: url('./assets/${selectElem.value}.png`;
+    // increment the state every time the user picks a different animal
+    animalCount++;
+    displayStats();
+});
+
+zodiacSelect.addEventListener('change', () => {
+    console.log('changing zodiac', zodiacSelect.value);
+    zodiacDiv.style.backgroundImage = `url('./assets/${zodiacSelect.value}.png')`;
+    zodiacCount++;
+    displayStats();
+});
+
+function displayStats() {
+    countElem.textContent = `You have changed the animal ${animalCount} times and the zodiac ${zodiacCount} times.`;
+}
